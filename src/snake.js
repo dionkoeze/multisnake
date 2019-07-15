13dc7e73c392be51eee369c6b27dff20961a3124
Dion Koeze <dion.koeze@gmail.com>
2019-07-16 00:25:47 +0200

    Implement drawing

--- src/snake.js
+++ src/snake.js
@@ -1,3 +1,18 @@
+const Direction = {
+    'Up': 1,
+    'Down': 2,
+    'Left': 3,
+    'Right': 4
+}
+
+const Colors = {
+    'Border': 'rgb(0, 0, 0)',
+    'Background': 'rgb(30, 30, 30)',
+    'SnakeHead': 'rgb(0, 180, 0)',
+    'SnakeBody': 'rgb(0, 140, 0)',
+    'Apple': 'rgb(180, 0, 0)'
+}
+
 class Coord {
     constructor(world, x, y) {
         this.world = world;
@@ -40,17 +55,64 @@ class Coord {
 }
 
 class Snake {
-    
+    constructor(world) {
+        this.world = world;
+        this.body = [];
+
+        // temp
+        this.body.push(new Coord(this.world, 10,5));
+        this.body.push(new Coord(this.world, 10,6));
+        this.body.push(new Coord(this.world, 11,6));
+        this.body.push(new Coord(this.world, 12,6));
+    }
+
+    step(move) {
+        if (move === 'up') {
+            moveUp();
+        } else if (move === 'down') {
+            moveDown();
+        } else if (move === 'left') {
+            moveLeft();
+        } else if (move === 'right') {
+            moveRight();
+        }
+    }
+
+    show(drawer) {
+        this.body.forEach((segment, idx) => {
+            let color;
+            if (idx === 0) {
+                color = Colors.SnakeHead;
+            } else {
+                color = Colors.SnakeBody;
+            }
+
+            drawer.drawCell(segment.x, segment.y, color);
+        })
+    }
 }
 
 class Apple {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
 
+    show(drawer) {
+        drawer.drawCell(this.x, this.y, Colors.Apple);
+    }
 }
 
 class World {
     constructor(sizeX, sizeY) {
         this.sizeX = sizeX;
         this.sizeY = sizeY;
+        this.snakes = [];
+        this.apples = [];
+
+        // temp
+        this.snakes.push(new Snake());
+        this.apples.push(new Apple(15, 17));
     }
 
     getCoord(x, y) {
@@ -76,4 +138,47 @@ class World {
     applyBoundaryY(y) {
         return applyBoundary(y, this.sizeY);
     }
+
+    getDrawer(canvas) {
+        return new Drawer(canvas, this.sizeX, this.sizeY);
+    }
+
+    show(drawer) {
+        drawer.reset();
+
+        for (let x = 0; x < this.sizeX; x++) {
+            for (let y = 0; y < this.sizeY; y++) {
+                // ctx.fillRect(x*strideX+border/2, y*strideY+border/2, strideX-border, strideY-border);
+                drawer.drawCell(x, y, Colors.Background);
+            }
+        }
+
+        this.snakes.forEach(snake => snake.show(drawer));
+        this.apples.forEach(apple => apple.show(drawer));
+    }
 }
+
+class Drawer {
+    constructor(canvas, sizeX, sizeY) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.strideX = canvas.width / sizeX;
+        this.strideY = canvas.height / sizeY;
+        this.border = Math.min(this.strideX, this.strideY)/8;
+    }
+
+    reset() {
+        this.ctx.fillStyle = Colors.Border;
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    drawCell(x, y, color) {
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect(x*this.strideX+this.border/2, y*this.strideY+this.border/2, this.strideX-this.border, this.strideY-this.border);
+    }
+}
+
+let world = new World(30, 30);
+let cnvs = document.getElementById("snakeCanvas");
+let drawer = world.getDrawer(cnvs);
+world.show(drawer);
